182435d93d805125d8a7ff89153030a5ce652da5
magicmaxmark <magicmaxmark@gmail.com>
2020-01-31 22:48:16 -0500

    Got Nodes Working When Q key is pressed

---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeScript : MonoBehaviour
+{
+	public bool startedLeft = false;
+	public bool controlledLeft = false;
+
+    public Sprite leftControlled;
+    public Sprite rightControlled;
+
+    public Sprite regionLeftControlled;
+    public Sprite regionRightControlled;
+
+    public GameObject region;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            changeControl();
+        }
+    }
+
+    void changeControl()
+    {
+        if (controlledLeft)
+        {
+            controlledLeft = false;
+            GetComponent<SpriteRenderer>().sprite = rightControlled;
+            region.GetComponent<SpriteRenderer>().sprite = regionRightControlled;
+
+        }
+
+        else if (!controlledLeft)
+        {
+            controlledLeft = true;
+            GetComponent<SpriteRenderer>().sprite = leftControlled;
+            region.GetComponent<SpriteRenderer>().sprite = regionLeftControlled;
+        }
+    }
+}
