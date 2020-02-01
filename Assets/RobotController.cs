dd789e7f63df86948baba061a56b209a729527e1
bigbroken <kennethmontgo@gmail.com>
2020-01-31 21:46:03 -0500

    Add FairyController and RobotController

---
+++ Assets/RobotController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 20.0f;
+    public enum SelectedPlayer {Player1,player2};
+    public SelectedPlayer Player;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Player == SelectedPlayer.Player1)
+        {
+            horizontal = Input.GetAxisRaw("Player1Horizontal");
+            vertical = Input.GetAxisRaw("Player1Vertical");
+        } else if(Player == SelectedPlayer.player2)
+        {
+            horizontal = Input.GetAxisRaw("Player2Horizontal");
+            vertical = Input.GetAxisRaw("Player2Vertical");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal, vertical).normalized * runSpeed;
+    }
+}
