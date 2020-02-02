004193703fe477c288685805900e7bb95c6285b1
TimothyAdan <brassbgeek@gmail.com>
2020-02-01 20:49:52 -0500

    Added Music Script and started playing music

---
+++ Assets/MusicStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicStart : MonoBehaviour
+{
+    public AK.Wwise.Event MyMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyMusic.Post(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
