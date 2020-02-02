51fb048bd476a40b71dfea31eaa4ccc75bc72ce9
TimothyAdan <brassbgeek@gmail.com>
2020-02-01 19:47:59 -0500

    Generated Soundbank to hopefully remove blip sound

--- Assets/Wwise_IDs.h
+++ Assets/Wwise_IDs.h
@@ -16,6 +16,82 @@ namespace AK
         static const AkUniqueID PLAY_BLIP = 1793296491U;
     } // namespace EVENTS
 
+    namespace STATES
+    {
+        namespace NODE_1
+        {
+            static const AkUniqueID GROUP = 4024353233U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAPTURED_FAIRY = 4188184641U;
+                static const AkUniqueID CAPTURED_ROBOT = 4029721826U;
+                static const AkUniqueID DESTROYED = 1359166010U;
+            } // namespace STATE
+        } // namespace NODE_1
+
+        namespace NODE_2
+        {
+            static const AkUniqueID GROUP = 4024353234U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAPTURED_FAIRY = 4188184641U;
+                static const AkUniqueID CAPTURED_ROBOT = 4029721826U;
+                static const AkUniqueID DESTROYED = 1359166010U;
+            } // namespace STATE
+        } // namespace NODE_2
+
+        namespace NODE_3
+        {
+            static const AkUniqueID GROUP = 4024353235U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAPTURED_FAIRY = 4188184641U;
+                static const AkUniqueID CAPTURED_ROBOT = 4029721826U;
+                static const AkUniqueID DESTROYED = 1359166010U;
+            } // namespace STATE
+        } // namespace NODE_3
+
+        namespace NODE_4
+        {
+            static const AkUniqueID GROUP = 4024353236U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAPTURED_FAIRY = 4188184641U;
+                static const AkUniqueID CAPTURED_ROBOT = 4029721826U;
+                static const AkUniqueID DESTROYED = 1359166010U;
+            } // namespace STATE
+        } // namespace NODE_4
+
+        namespace NODE_5
+        {
+            static const AkUniqueID GROUP = 4024353237U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAPTURED_FAIRY = 4188184641U;
+                static const AkUniqueID CAPTURED_ROBOT = 4029721826U;
+                static const AkUniqueID DESTROYED = 1359166010U;
+            } // namespace STATE
+        } // namespace NODE_5
+
+        namespace NODE_6
+        {
+            static const AkUniqueID GROUP = 4024353238U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAPTURED_FAIRY = 4188184641U;
+                static const AkUniqueID CAPTURED_ROBOT = 4029721826U;
+                static const AkUniqueID DESTROYED = 1359166010U;
+            } // namespace STATE
+        } // namespace NODE_6
+
+    } // namespace STATES
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
