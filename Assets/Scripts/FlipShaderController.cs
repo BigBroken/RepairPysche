01c1615dd6f2abc49a25bf380ab739ae5b17b0d7
Brian Kendall <brian@briankendall.net>
2020-02-01 23:33:56 -0500

    Made flip shader

---
+++ Assets/Scripts/FlipShaderController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FlipShaderController : MonoBehaviour
+{
+    public Camera targetCamera = null;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Vector3 screenPos = targetCamera.WorldToScreenPoint(transform.position);
+        renderer.material.SetVector("_ObjectPosition", screenPos);
+    }
+}
