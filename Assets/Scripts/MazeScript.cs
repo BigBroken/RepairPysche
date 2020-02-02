68b09da34950a80d2245dd24753e3f610fa18fe4
magicmaxmark <magicmaxmark@gmail.com>
2020-02-01 22:49:21 -0500

    Maze Assets added working on scripting now

---
+++ Assets/Scripts/MazeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeScript : MonoBehaviour
+{
+    GameObject[] tiles = { };
+    float[] yPositions = { 0.59f, -0.6f, -0.72f };
+    float xMax = -0.178f;
+    float xMin = 0.178f;
+
+    private void Start()
+    {
+        GenerateMaze();
+    }
+
+    public void GenerateMaze()
+    {
+        
+    }
+
+    public void EndMaze()
+    {
+
+    }
+}
